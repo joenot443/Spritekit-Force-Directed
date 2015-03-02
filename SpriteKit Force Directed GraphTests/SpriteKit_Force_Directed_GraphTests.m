0df200f97c3f6a5062bb595c703757b2c2e24614
joenot443 <joe.crozier@live.ca>
2015-03-02 13:15:32 -0500

    Added remaining project files

---
+++ SpriteKit Force Directed GraphTests/SpriteKit_Force_Directed_GraphTests.m
@@ -0,0 +1,40 @@
+//
+//  SpriteKit_Force_Directed_GraphTests.m
+//  SpriteKit Force Directed GraphTests
+//
+//  Created by Joe Crozier on 2015-03-02.
+//  Copyright (c) 2015 Joe Crozier Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SpriteKit_Force_Directed_GraphTests : XCTestCase
+
+@end
+
+@implementation SpriteKit_Force_Directed_GraphTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
